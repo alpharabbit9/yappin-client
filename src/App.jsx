38feaccbf3ff10bb3ec2af25c5a9c007047952be
alpharabbit9 <alpharabbit74@gmail.com>
2025-09-24 00:36:51 +0600

    First commit

---
+++ src/App.jsx
@@ -0,0 +1,26 @@
+import React from 'react'
+import { Route, Routes } from 'react-router'
+import ChatPage from './pages/ChatPage'
+import SignUp from './pages/SignUp'
+import LoginPage from './pages/LoginPage'
+
+const App = () => {
+  return (
+    <div className="min-h-screen bg-slate-900 relative flex items-center justify-center p-4 overflow-hidden ">
+
+      <div className="absolute inset-0 bg-[linear-gradient(to_right,#4f4f4f2e_1px,transparent_1px),linear-gradient(to_bottom,#4f4f4f2e_1px,transparent_1px)] bg-[size:14px_24px]" />
+      <div className="absolute top-0 -left-4 size-96 bg-pink-500 opacity-20 blur-[100px]" />
+      <div className="absolute bottom-0 -right-4 size-96 bg-cyan-500 opacity-20 blur-[100px]" />
+
+
+      <Routes>
+        <Route path="/" element={<ChatPage />} />
+        <Route path="/signup" element={<SignUp />} />
+        <Route path="/login" element={<LoginPage />} />
+      </Routes>
+    </div>
+
+  )
+}
+
+export default App
