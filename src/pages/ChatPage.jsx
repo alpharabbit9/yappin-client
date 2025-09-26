38e569b146d4320744cdc4769b963bb8cbf96b8f
alpharabbit9 <alpharabbit74@gmail.com>
2025-09-26 22:51:22 +0600

    added chat Page

--- src/pages/ChatPage.jsx
+++ src/pages/ChatPage.jsx
@@ -1,8 +1,48 @@
 import React from 'react'
+import BorderAnimatedContainer from '../components/BorderAnimatedContainer'
+import { useChatStore } from '../store/useChatStore'
+import ProfileHeader from '../components/ProfileHeader'
+import ActiveTabSwitch from '../components/ActiveTabSwitch'
+import ChatList from '../components/ChatList'
+import ContactList from '../components/ContactList'
+import ChatContainer from '../components/ChatContainer'
+import NoConversationPlaceHolder from '../components/NoConversationPlaceHolder'
 
 const ChatPage = () => {
+
+  const {activeTab , selectedUser} = useChatStore()
   return (
-    <div>ChatPage</div>
+    <div className='relative w-full max-w-6xl h-[600px]'>
+
+      <BorderAnimatedContainer>
+
+        {/* Left Side  */}
+
+        <div className='w-80 bg-slate-800/50 backdrop-blur-sm flex flex-col'>
+
+        <ProfileHeader/>
+        <ActiveTabSwitch/>
+
+
+        <div className='flex-1 overflow-y-auto p-4 space-y-2'>
+
+          {activeTab === "chats" ? <ChatList/> : <ContactList/>}
+
+
+
+        </div>
+
+        </div>
+
+
+        {/* Right Side */}
+
+        <div className='flex-1 flex flex-col bg-slate-900/50 backdrop-blur-sm'>
+{selectedUser ? <ChatContainer/> : <NoConversationPlaceHolder/>}
+        </div>
+      </BorderAnimatedContainer>
+
+    </div>
   )
 }
 
